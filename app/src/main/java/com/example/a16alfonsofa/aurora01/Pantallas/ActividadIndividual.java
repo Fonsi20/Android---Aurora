0d13dce4c4b6fd2886174c872f94399355e25705
Alfonso Fernández Álvarez <fonsi20fernandez@gmail.com>
2019-04-15 11:37:31 +0200

    pantalla actividad individual

---
+++ app/src/main/java/com/example/a16alfonsofa/aurora01/Pantallas/ActividadIndividual.java
@@ -0,0 +1,15 @@
+package com.example.a16alfonsofa.aurora01.Pantallas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.a16alfonsofa.aurora01.R;
+
+public class ActividadIndividual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_individual);
+    }
+}
