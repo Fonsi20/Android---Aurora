2f950cc1492983f3469b0970f30dd81e863165e7
Fonsi <alfonso@crescent-integrations.com>
2019-05-17 10:52:20 +0200

    Metodo random instaurado

--- app/src/main/java/com/example/a16alfonsofa/aurora01/Pantallas/ActividadIndividual.java
+++ app/src/main/java/com/example/a16alfonsofa/aurora01/Pantallas/ActividadIndividual.java
@@ -5,11 +5,32 @@ import android.os.Bundle;
 
 import com.example.a16alfonsofa.aurora01.R;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class ActividadIndividual extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_individual);
+
+        Random r = new Random();
+        Set<Integer> generados = new HashSet<>();
+        for (int i = 1; i <= 20; i++) {
+            int aleatorio = -1;
+            boolean generado = false;
+            while (!generado) {
+                int posible = r.nextInt();
+                if (!generados.contains(posible)) {
+                    generados.add(posible);
+                    aleatorio = posible;
+                    generado = true;
+                }
+            }
+            //usa el valor de aleatorio...
+
+        }
     }
 }
